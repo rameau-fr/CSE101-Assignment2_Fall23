95d929d7449024898b0252728ec05790f74cc481
Francois Rameau <rameau.fr@gmail.com>
2023-10-09 13:50:58 +0900

    first commit

---
+++ Homework_2.py
@@ -0,0 +1,60 @@
+def view_tasks(todo_list):
+    """ Display all the tasks in the ToDo list
+    
+    Args:
+        todo_list (list): A list containing tasks and their priorities as sublists.
+    
+    Returns:
+        None
+    """
+    ...
+
+def search_tasks(todo_list):
+    """ Search and display tasks containing a specific keyword
+    
+    Args:
+        todo_list (list): A list containing tasks and their priorities as sublists.
+    
+    Returns:
+        None
+    """
+    ...
+
+def add_task(todo_list):
+    """ Add a task to the ToDo list
+    
+    Args:
+        todo_list (list): A list containing tasks and their priorities as sublists.
+    
+    Returns:
+        list: Updated ToDo list
+    """
+    ...
+
+def remove_task(todo_list):
+    """ Remove a task from the ToDo list
+    
+    Args:
+        todo_list (list): A list containing tasks and their priorities as sublists.
+    
+    Returns:
+        list: Updated ToDo list
+    """
+    ...
+
+def display_menu():
+    """ Display the menu for the ToDo list software
+    
+    Returns:
+        int: User's choice corresponding to the operations they want to perform.
+    """
+    ...
+    
+
+def main():
+    todo_list = [] #initialize empty todolist
+    while True: # main software loop
+        ...
+
+if __name__ == "__main__": # Run the main!
+    main()
